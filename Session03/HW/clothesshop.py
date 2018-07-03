14e18282b2a5b97d387dc2bc279ed2c5fca441f9
Nguyen Thi Ha Thu <hathu0610@Nguyens-MacBook-Air.local>
2018-07-03 10:54:12 +0700

    inital commit

--- Session03/HW/clothesshop.py
+++ Session03/HW/clothesshop.py
@@ -3,18 +3,13 @@ list_items = ["T-Shirt","Sweater"]
 while condition == True:
     question = input("Welcome to our shop, what do you want (C, R, U, D)?")
 
-   
-
-
-
-    if question == 'R':
+    if question == 'R' or question =='r':
         condition = True
-    if question == 'C':
+    elif question == 'C' or question =='c':
         new_items = input("Enter new item:")
         list_items.append(new_items)
-        condition = True
-        
-    if question == 'U':
+        condition = True    
+    elif question == 'U' or question =='u':
         update_position = int(input("Update position?"))
         if update_position > len(list_items):
             print("The position you want to update do not exist!")
@@ -22,8 +17,7 @@ while condition == True:
         replace_item = input("New item?")
         list_items[update_position-1] = replace_item
         condition = True
-
-    if question == 'D':
+    elif question == 'D' or question =='d':
         dele_posi = int(input("Delete position?"))
         
         if dele_posi > len(list_items):
@@ -31,7 +25,10 @@ while condition == True:
             continue
         list_items.pop(dele_posi - 1)
         condition = True
-    print("Our items:", ",".join(list_items))   
+    else: 
+        print("Your input is not valid!!")
+        continue
+    print("Our items:", ",".join(list_items))    
     n = input("Would you like to continue?")
     if n == 'yes':
         condition = True
