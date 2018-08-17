3d304d40a38172730c7fa8f760cefd200b83a86e
Nguyen Thi Ha Thu <hathu0610@Nguyens-MacBook-Air.local>
2018-08-17 14:52:22 +0700

    sozy nop muon

--- Web02/pilot-app/populate.py
+++ Web02/pilot-app/populate.py
@@ -9,7 +9,7 @@ fake = Faker()
 # print(fake.address())
 
     
-image_link =["https://bit.ly/2OLDjhE", "https://bit.ly/2AMXvwR", "https://bit.ly/2nhxQ5L", "https://bit.ly/2KziXVz"]
+image_link =["http://bit.ly/2nHXuAP","https://scontent.fhan5-7.fna.fbcdn.net/v/t1.0-9/38492579_1786246014804552_3611468412422717440_n.jpg?_nc_cat=0&oh=d035f8848b1ca0d70a21c575b3714c27&oe=5C060DE2"]
 for image in image_link:
     ran_description = sample(["ngoan hiền", "dễ thương", "lễ phép với gia đình", "cute", "thích uống trà sữa", "thích đi chơi", "ngủ nhiều"], 3)
     ran_mea = [randint(80,100), randint(50,70), randint(80,100)]
